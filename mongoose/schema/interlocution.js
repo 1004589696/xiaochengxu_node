a9d5519a9d92982a7a12b2097b59d1a8e69dcba8
Administrator <cunkuan.ding@tenbent.com>
2018-05-31 13:59:51 +0800

    mongoose db

---
+++ mongoose/schema/interlocution.js
@@ -0,0 +1,28 @@
+/**
+ *  interlocution 信息
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var db = require('../db');
+
+
+var Interlocution = new Schema({
+    questionType: {type: String},
+    questionLevel: {type: String},
+    questionName: {type: String},
+    answerList: [{
+        answerContent: {type: String, require: true},
+        is_answer: {type: Boolean, require: true},
+        status: {type: Number, default: 0},
+        createTime: {type: Date, default: Date.now},
+    }],
+    createTime: {type: Date, default: Date.now},
+    updateTime: {type: Date, default: Date.now},
+}, {
+    versionKey: false,
+    timestamps: {createdAt: 'createTime', updatedAt: 'updateTime'}
+});
+
+
+// 编译模型
+module.exports = db.XIAOCHENGXU_NODE.model('Interlocution', Interlocution);
--- routes/interlocution.js
+++ routes/interlocution.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var Interlocution = require("../mongoose/schema/interlocution");
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+/**
+ * 实现问题添加
+ */
+router.post('/add', function (req, res, next) {
+    var reqData = req.body;
+    var InterlocutionObj = new Interlocution(reqData);
+    InterlocutionObj.save(function (err, result) {
+        if (err) {
+            res.json({
+                code: '500',
+                msg: "Error:" + err
+            });
+        } else {
+            res.json({
+                code: '0',
+                result: result
+            });
+        }
+    })
 });
+
+/**
+ * 实现问题编辑
+ */
+router.post('/edit', function (req, res, next) {
+    var interlocutionId = req.query.interlocutionId;
+    var reqData = req.body;
+    Interlocution.update({_id: interlocutionId}, reqData, function (err, result) {
+        if (err) {
+            res.json({
+                code: '500',
+                msg: "Error:" + err
+            });
+        } else {
+            res.json({
+                code: '0',
+                result: result
+            });
+        }
+    });
+});
+
 
 module.exports = router;
